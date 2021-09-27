4655c5bbda923f2032d76c037a32f836d58110b8
Matt C <matt@yhat.pub>
2021-09-27 16:59:23 -0700

    Reverting to non ecs pipeline

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -2,13 +2,17 @@ apt install -y nodejs
 apt install -y npm
 npm install pm2@latest -g
 
-if [ "$APPLICATION_NAME" == "builder" ]
+sudo systemctl stop apache2
+
+if [ "$APPLICATION_NAME" == "YHatBuilder" ]
 then
-    apt install -y awscli
+    sudo apt install -y awscli
     echo '{"max-concurrent-uploads": 1 }' > /etc/docker/daemon.json
 
-    systemctl daemon-reload
-    systemctl restart docker
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+
+    sudo systemctl start apache2
 
     $(aws ecr get-login --region us-east-1 --no-include-email)
     docker pull public.ecr.aws/c6h1o1s4/inference_lambda_public:base_pytorch
--- deployment_scripts/start_server.sh
+++ deployment_scripts/start_server.sh
@@ -1,9 +1,9 @@
-if [ "$APPLICATION_NAME" == "builder" ]
+if [ "$APPLICATION_NAME" == "YHatBuilder" ]
 then
     pm2 start "venv/bin/python -m app.service.builder_server.builder_server" --name builder
 fi
 
-if [ "$APPLICATION_NAME" == "fastapi" ]
+if [ "$APPLICATION_NAME" == "YHatFastApi" ]
 then
     pm2 start "venv/bin/python -m uvicorn app.api:app  --host 0.0.0.0 --port 8000" --name fastapi
 fi
